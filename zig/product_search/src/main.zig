7c339a9183e1ba8e7c415a89f66a3411daff4cc1
Ben <bnjox@users.noreply.github.com>
2026-01-27 11:55:46 +0000

    fix: add defer to free memory

--- zig/product_search/src/main.zig
+++ zig/product_search/src/main.zig
@@ -7,21 +7,29 @@ const Product = struct {
 };
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
     var product_buffer: [100]u8 = undefined;
     var stdin = std.fs.File.stdin();
+
+    var products: std.ArrayList(Product) = .empty;
+    defer products.deinit(allocator);
 
     var file_buffer: [1024]u8 = undefined;
     const contents = try std.fs.cwd().readFile("./src/products.json", &file_buffer);
 
-    const parsed = try std.json.parseFromSlice(struct { products: []Product }, std.heap.smp_allocator, contents, .{});
-    const products: []Product = parsed.value.products;
+    const parsed = try std.json.parseFromSlice(struct { products: []Product }, allocator, contents, .{});
+    defer parsed.deinit();
+    try products.appendSlice(allocator, @as([]Product, parsed.value.products));
 
     outer: while (true) {
         std.debug.print("What is the product name? ", .{});
         var reader = stdin.reader(&product_buffer);
         const product_name = try reader.interface.takeDelimiterExclusive('\n');
 
-        for (products) |product| {
+        for (products.items) |product| {
             if (std.ascii.eqlIgnoreCase(product.name, product_name)) {
                 std.debug.print("Name: {s}\nPrice: ${d:.2}\nQuantity on hand: {d}\n", .{ product.name, product.price, product.quantity });
                 break :outer;
