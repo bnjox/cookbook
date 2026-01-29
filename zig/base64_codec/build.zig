1242aa12022ce4cc9b45a3fefb053820ae8b0025
Ben <bnjox@users.noreply.github.com>
2026-01-29 14:47:33 +0000

    add build.zig files

---
+++ zig/base64_codec/build.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "base64_codec",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the base64_codec");
+    run_step.dependOn(&exe.step);
+}
